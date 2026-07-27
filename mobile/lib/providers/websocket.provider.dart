immich-app/immich

fix(mobile): refresh snapshot views after remote updates

mobile/lib/providers/websocket.provider.dart
@@ -8,6 +8,7 @@ import 'package:immich_mobile/models/server_info/server_version.model.dart';
 import 'package:immich_mobile/providers/auth.provider.dart';
 import 'package:immich_mobile/providers/background_sync.provider.dart';
 import 'package:immich_mobile/providers/server_info.provider.dart';
+import 'package:immich_mobile/providers/sync_status.provider.dart';
 import 'package:immich_mobile/utils/debounce.dart';
 import 'package:immich_mobile/utils/debug_print.dart';
 import 'package:logging/logging.dart';
@@ -169,7 +170,11 @@ class WebsocketNotifier extends StateNotifier<WebsocketState> {
   }
 
   void _handleSyncAssetEditReady(dynamic data) {
-    unawaited(_ref.read(backgroundSyncProvider).syncWebsocketEdit(data));
+    unawaited(
+      _ref.read(backgroundSyncProvider).syncWebsocketEdit(data).then((_) {
+        _ref.read(syncStatusProvider.notifier).markRemoteContentChanged();
+      }),
+    );
   }
 
   void _processBatchedAssetUploadReady() {
@@ -181,6 +186,7 @@ class WebsocketNotifier extends StateNotifier<WebsocketState> {
     try {
       unawaited(
         _ref.read(backgroundSyncProvider).syncWebsocketBatch(_batchedAssetUploadReady.toList()).then((_) {
+          _ref.read(syncStatusProvider.notifier).markRemoteContentChanged();
           if (isSyncAlbumEnabled) {
             _ref.read(backgroundSyncProvider).syncLinkedAlbum();
           }

BierDav/noodle-gallery

mobile/lib/providers/websocket.provider.dart
@@ -9,6 +9,7 @@ import 'package:immich_mobile/providers/auth.provider.dart';
 import 'package:immich_mobile/providers/background_sync.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/settings.provider.dart';
 import 'package:immich_mobile/providers/server_info.provider.dart';
+import 'package:immich_mobile/providers/sync_status.provider.dart';
 import 'package:immich_mobile/utils/debounce.dart';
 import 'package:immich_mobile/utils/debug_print.dart';
 import 'package:logging/logging.dart';
@@ -182,7 +183,11 @@ class WebsocketNotifier extends StateNotifier<WebsocketState> {
   }
 
   void _handleSyncAssetEditReadyV1(dynamic data) {
-    unawaited(_ref.read(backgroundSyncProvider).syncWebsocketEditV1(data));
+    unawaited(
+      _ref.read(backgroundSyncProvider).syncWebsocketEditV1(data).then((_) {
+        _ref.read(syncStatusProvider.notifier).markRemoteContentChanged();
+      }),
+    );
   }
 
   void _handleAlbumUpdate(dynamic _) {
@@ -190,7 +195,11 @@ class WebsocketNotifier extends StateNotifier<WebsocketState> {
   }
 
   void _handleSyncAssetEditReadyV2(dynamic data) {
-    unawaited(_ref.read(backgroundSyncProvider).syncWebsocketEditV2(data));
+    unawaited(
+      _ref.read(backgroundSyncProvider).syncWebsocketEditV2(data).then((_) {
+        _ref.read(syncStatusProvider.notifier).markRemoteContentChanged();
+      }),
+    );
   }
 
   void _processBatchedAssetUploadReadyV1() {
@@ -202,6 +211,7 @@ class WebsocketNotifier extends StateNotifier<WebsocketState> {
     try {
       unawaited(
         _ref.read(backgroundSyncProvider).syncWebsocketBatchV1(_batchedAssetUploadReady.toList()).then((_) {
+          _ref.read(syncStatusProvider.notifier).markRemoteContentChanged();
           if (isSyncAlbumEnabled) {
             _ref.read(backgroundSyncProvider).syncLinkedAlbum();
           }
@@ -223,6 +233,7 @@ class WebsocketNotifier extends StateNotifier<WebsocketState> {
     try {
       unawaited(
         _ref.read(backgroundSyncProvider).syncWebsocketBatchV2(_batchedAssetUploadReady.toList()).then((_) {
+          _ref.read(syncStatusProvider.notifier).markRemoteContentChanged();
           if (isSyncAlbumEnabled) {
             _ref.read(backgroundSyncProvider).syncLinkedAlbum();
           }