immich-app/immich

feat: support direct S3 media delivery (#502)
* feat: sign s3 media response metadata

* feat: preserve filenames for original downloads

* fix: bypass service worker for asset media

* fix: load redirected media with cors

* fix: probe cast media with ranged get

* docs: document s3 redirect media delivery

* chore: update openapi for original downloads

* chore: update mobile openapi client

* chore: format s3 storage backend

* test: fix web e2e media mocks

web/src/lib/utils.spec.ts
@@ -1,9 +1,37 @@
-import { getAssetUrl, getMemoryTitle, getReleaseType } from '$lib/utils';
-import { AssetTypeEnum, MemoryType, type MemoryResponseDto } from '@immich/sdk';
+import { getAssetMediaUrl, getAssetUrl, getMemoryTitle, getReleaseType } from '$lib/utils';
+import { AssetMediaSize, AssetTypeEnum, MemoryType, type MemoryResponseDto } from '@immich/sdk';
 import { assetFactory } from '@test-data/factories/asset-factory';
 import { sharedLinkFactory } from '@test-data/factories/shared-link-factory';
 
 describe('utils', () => {
+  describe(getAssetMediaUrl.name, () => {
+    it('adds download=true to original media URLs when requested', () => {
+      const url = getAssetMediaUrl({
+        id: 'asset-1',
+        size: AssetMediaSize.Original,
+        edited: false,
+        cacheKey: 'cache-1',
+        download: true,
+      });
+
+      expect(url).toContain('/api/assets/asset-1/original');
+      expect(url).toContain('download=true');
+      expect(url).toContain('edited=false');
+      expect(url).toContain('c=cache-1');
+    });
+
+    it('does not add download=true to thumbnail media URLs', () => {
+      const url = getAssetMediaUrl({
+        id: 'asset-1',
+        size: AssetMediaSize.Thumbnail,
+        download: true,
+      });
+
+      expect(url).toContain('/api/assets/asset-1/thumbnail');
+      expect(url).not.toContain('download=true');
+    });
+  });
+
   describe(getAssetUrl.name, () => {
     it('should return thumbnail URL for static images', () => {
       const asset = assetFactory.build({

BierDav/noodle-gallery

web/src/lib/utils.spec.ts
@@ -1,9 +1,37 @@
-import { AssetTypeEnum, MemoryType, type MemoryResponseDto } from '@immich/sdk';
-import { getAssetUrl, getMemoryTitle, semverToName } from '$lib/utils';
+import { AssetMediaSize, AssetTypeEnum, MemoryType, type MemoryResponseDto } from '@immich/sdk';
+import { getAssetMediaUrl, getAssetUrl, getMemoryTitle, semverToName } from '$lib/utils';
 import { assetFactory } from '@test-data/factories/asset-factory';
 import { sharedLinkFactory } from '@test-data/factories/shared-link-factory';
 
 describe('utils', () => {
+  describe(getAssetMediaUrl.name, () => {
+    it('adds download=true to original media URLs when requested', () => {
+      const url = getAssetMediaUrl({
+        id: 'asset-1',
+        size: AssetMediaSize.Original,
+        edited: false,
+        cacheKey: 'cache-1',
+        download: true,
+      });
+
+      expect(url).toContain('/api/assets/asset-1/original');
+      expect(url).toContain('download=true');
+      expect(url).toContain('edited=false');
+      expect(url).toContain('c=cache-1');
+    });
+
+    it('does not add download=true to thumbnail media URLs', () => {
+      const url = getAssetMediaUrl({
+        id: 'asset-1',
+        size: AssetMediaSize.Thumbnail,
+        download: true,
+      });
+
+      expect(url).toContain('/api/assets/asset-1/thumbnail');
+      expect(url).not.toContain('download=true');
+    });
+  });
+
   describe(getAssetUrl.name, () => {
     it('should return thumbnail URL for static images', () => {
       const asset = assetFactory.build({