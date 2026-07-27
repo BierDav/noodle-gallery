inhotel-io/inreveal

fix(mobile): load a shared-space person's photos on the person-detail page (both entry points) (#738)
* fix(mobile): load a shared-space person's photos on the person detail page

Tapping a shared-space person opened an empty '0 items' detail page on
mobile while web showed their photos. The mobile person-detail timeline
was built from owner-scoped local Drift data (remote_asset.ownerId ==
viewer joined to asset_face.personId), which is empty for a person the
viewer doesn't own: a space person's photos are owned by another user
and their face->person links are owner-scoped and never sync.

Route the detail timeline on DriftPerson.spaceId (buildPersonTimeline
RouteService), exactly like web's person-detail page. A personal person
keeps the local owner-scoped timeline; a space person fetches its photo
ids from the already membership-gated GET /shared-spaces/{id}/people/
{personId}/assets and renders them from the locally-synced space assets
via DriftTimelineRepository.sharedSpacePerson (remote_asset.id IN ids,
no ownerId filter, no asset_face join). Best-effort: a fetch failure or
in-flight load yields an empty timeline, never a fall-through to the
owner-scoped query.

Mobile-only; no server or RBAC change (the endpoint is already
membership-gated and web already exposes exactly these photos). Sibling
of #727 (per-photo faces) and #737 (People page grid).

* fix(mobile): load a shared-space person's photos when tapping a face in a photo

Folds the face-tap entry point into the person-detail space fix so the
detail page works from BOTH the People page (#737) and from tapping a
face in a photo's info sheet.

The person-detail timeline routes on DriftPerson.spaceId, but the
face-tap path builds its DriftPerson via the asset-info mapper
(person_api.repository.dart _toDriftPerson), which set id = the GLOBAL
person id and left spaceId null — so the space branch never fired and it
fell through to the empty owner-scoped query (0 photos). The asset-info
DTO carries the space-p

mobile/lib/repositories/person_api.repository.dart
@@ -122,16 +122,26 @@ class PersonApiRepository extends ApiRepository {
     final info = await checkNull(_apiService.assetsApi.getAssetInfo(assetId));
     return info.people
         .where((person) => !person.isHidden)
-        .map((person) => _toDriftPerson(person, info.ownerId))
+        .map((person) => _toDriftPerson(person, info.ownerId, info.resolvedSpaceId))
         .toList();
   }
 
-  static DriftPerson _toDriftPerson(PersonWithFacesResponseDto dto, String ownerId) {
+  static DriftPerson _toDriftPerson(PersonWithFacesResponseDto dto, String ownerId, String? resolvedSpaceId) {
     // The asset-info DTO does not carry created/faceAsset fields; the people strip does
     // not render them, so mirror updatedAt and leave the face-asset unset.
     final updatedAt = dto.updatedAt ?? DateTime.fromMillisecondsSinceEpoch(0);
+    // Face-tap → person detail: for a Space-shared person the asset-info endpoint carries the
+    // space-person id (dto.spacePersonId) separately from the global identity id (dto.id), and
+    // the space id lives on the asset (resolvedSpaceId). Map such a person shape-identical to
+    // the People-page one — id = space-person id, spaceId set — so buildPersonTimelineRouteService
+    // takes the space branch and the detail page loads photos (and the thumbnail routes to the
+    // membership-gated space endpoint), mirroring web. Both ids must be present: the space assets
+    // endpoint needs the (spaceId, space-person id) pair. Personal/owned people keep the global id
+    // and null spaceId (owner-scoped local query + owner thumbnail). See issue #727.
+    final spacePersonId = dto.spacePersonId;
+    final isSpacePerson = spacePersonId != null && resolvedSpaceId != null;
     return DriftPerson(
-      id: dto.id,
+      id: isSpacePerson ? spacePersonId : dto.id,
       createdAt: updatedAt,
       updatedAt: updatedAt,
       ownerId: ownerId,
@@ -140,6 +150,7 @@ class PersonApiRepository extends ApiRepository {
       isHidden: dto.isHidden,
       color: dto.color,
       birthDate: dto.birthDate,
+      spaceId: isSpacePerson ? resolvedSpaceId : null,
     );
   }
 

BierDav/noodle-gallery

mobile/lib/repositories/person_api.repository.dart
@@ -122,16 +122,29 @@ class PersonApiRepository extends ApiRepository {
   Future<List<DriftPerson>> getAssetPeople(String assetId) async {
     final info = await checkNull(_apiService.assetsApi.getAssetInfo(assetId));
     final people = info.people.orElse(null) ?? const <PersonResponseDto>[];
-    return people.where((person) => !person.isHidden).map((person) => _toDriftPerson(person, info.ownerId)).toList();
+    return people
+        .where((person) => !person.isHidden)
+        .map((person) => _toDriftPerson(person, info.ownerId, info.resolvedSpaceId.orElse(null)))
+        .toList();
   }
 
-  static DriftPerson _toDriftPerson(PersonResponseDto dto, String ownerId) {
+  static DriftPerson _toDriftPerson(PersonResponseDto dto, String ownerId, String? resolvedSpaceId) {
     // The asset-info DTO does not carry created/faceAsset fields; the people strip does
     // not render them, so mirror updatedAt and leave the face-asset unset.
     // v3 openapi wraps optional person fields in Optional<...?> → unwrap with orElse.
     final updatedAt = dto.updatedAt.orElse(null) ?? DateTime.fromMillisecondsSinceEpoch(0);
+    // Face-tap → person detail: for a Space-shared person the asset-info endpoint carries the
+    // space-person id (dto.spacePersonId) separately from the global identity id (dto.id), and
+    // the space id lives on the asset (resolvedSpaceId). Map such a person shape-identical to
+    // the People-page one — id = space-person id, spaceId set — so buildPersonTimelineRouteService
+    // takes the space branch and the detail page loads photos (and the thumbnail routes to the
+    // membership-gated space endpoint), mirroring web. Both ids must be present: the space assets
+    // endpoint needs the (spaceId, space-person id) pair. Personal/owned people keep the global id
+    // and null spaceId (owner-scoped local query + owner thumbnail). See issue #727.
+    final spacePersonId = dto.spacePersonId.orElse(null);
+    final isSpacePerson = spacePersonId != null && resolvedSpaceId != null;
     return DriftPerson(
-      id: dto.id,
+      id: isSpacePerson ? spacePersonId : dto.id,
       createdAt: updatedAt,
       updatedAt: updatedAt,
       ownerId: ownerId,
@@ -140,6 +153,7 @@ class PersonApiRepository extends ApiRepository {
       isHidden: dto.isHidden,
       color: dto.color.orElse(null),
       birthDate: dto.birthDate,
+      spaceId: isSpacePerson ? resolvedSpaceId : null,
     );
   }
 